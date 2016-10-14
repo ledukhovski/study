ec46777e5a9703d1964f0c45856e5a7371ad368b
ledukhovski.s <ledukhovski.s@biz-apps.ru>
2016-10-14 15:06:48 +0300

    no message

--- SQL_SRV_STUDY/SQL_SRV_STUDY/Pivot.sql
+++ SQL_SRV_STUDY/SQL_SRV_STUDY/Pivot.sql
@@ -1,5 +1,7 @@
 USE TSQL2012;
 
+
+
 /*Write a PIVOT query against the Sales.Orders table that returns the maximum shipping
 date for each order year and shipper ID. Return order years on rows, shipper IDs (1, 2,
 and 3) on columns, and the maximum shipping dates in the data part.*/
@@ -23,13 +25,17 @@ FOR < spreading column > IN (< distinct spreading values >) ) AS P;
 
 WITH Pvt AS
 (
-SELECT year(o.orderdate), o.shipperid, o.shippeddate
+SELECT year(o.orderdate) AS [Year], o.shipperid AS [ShipperId], o.shippeddate AS [DATE]
  FROM Sales.Orders o
 )
-
-
-SELECT * FROM pvt;
-
 SELECT *
  from Pvt p
- PIVOT ( max(o.shippeddate)
+ PIVOT ( max([DATE]) FOR shipperid IN ([1], [2], [3])) as A;
+
+
+ SELECT Customer.custid AS [@custid],
+Customer.companyname AS [companyname]
+FROM Sales.Customers AS Customer
+WHERE Customer.custid <= 2
+ORDER BY Customer.custid
+FOR XML PATH ('Customer'), ROOT('Customers');
